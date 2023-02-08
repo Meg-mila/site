96fc43463231674219af4ca38c8e53b15e3aa1f3
Margarita <margarita@macbooks-mbp.mynet>
2023-02-08 18:15:28 +0000

    add ga events

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('a')
     links.forEach(link => {
         link.addEventListener('click', function (ev) {
-            const event = ev.target.dataset.metric;
+            const event = ev.currentTarget.dataset.metric;
             if (window.gtag && event) {
                 window.gtag('event', 'click_link', {
                     'data_metric': event,
